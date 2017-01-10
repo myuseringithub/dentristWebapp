4af1663cad168c8baef6a04f420820d2acd53cde
MyUserInGithub <sfmissive@gmail.com>
2017-01-10 13:35:37 +0200

    WORKS - Updated setup files.

--- setup/shellScript/addContentAndConfigs.sh
+++ setup/shellScript/addContentAndConfigs.sh
@@ -8,7 +8,6 @@
 # # execute CMD
 # exec "$@"
 
-
 # 😄 SZN:
 echo "SZN - Content & Configurations for Apache, PHP, & Wordpress";
 # Run only when folders are empty:
@@ -37,9 +36,8 @@ else
     # Directory must not exist (in order to copy hidden files).
     # mkdir -p /app/root/; 
     cp -pr /tmp/source/clientSide/root/ /app/root/
-    mkdir -p /app/root/assets/; cp -pr /tmp/source/clientSide/assets/ /app/root/assets/
     # SZN COMPATIBILITY - changing "app" to "assets" throws an error 500 in "/mcq" page. (if folder doesn't exist, its created and teh contents of the source are copied into it)
-    mv /app/root/assets /app/root/app
+    mkdir -p /app/root/app/; cp -pr /tmp/source/clientSide/assets/* /app/root/app/
     # wp-content
     mkdir -p /app/root/content/; # cp -pr /tmp/content/wp-content/* /app/root/content/
     mkdir -p /app/root/content/mu-plugins; cp -pr /tmp/source/serverSide/wordpressPlugins_mustUsePlugins/* /app/root/content/mu-plugins/
@@ -86,17 +84,19 @@ else
 
     #⭐ JSPM Libraries (Javascript libraries) - 
     # install JSPM using npm: works only with beta version 0.17.0-beta.25 https://github.com/jspm/jspm-cli/releases/tag/0.17.0-beta.4
+    curl -sL https://deb.nodesource.com/setup_7.x | bash -
+    apt-get install -y nodejs
     npm install -g jspm@beta
-    # Bower Endpoint to allow installation from Bower sources
+    # Bower Endpoint to allow installation from Bower sources - for beta version there is no need for bower endpoint
     npm install -g jspm-bower-endpoint
-    jspm registry create -y bower jspm-bower-endpoint
+    jspm registry create bower jspm-bower-endpoint
     # Install NPM & JSPM packages:
     (cd /tmp/source/clientSide/jspm_packages_modules/; \
     jspm install)
     # move to desired location: 
-    mv /tmp/source/clientSide/jspm_packages_modules/jspm_packages/ /app/root/app/sharedApp/javascripts/
+    mkdir -p /app/root/app/sharedApp/javascripts/jspm_packages/;
+    mv /tmp/source/clientSide/jspm_packages_modules/jspm_packages/* /app/root/app/sharedApp/javascripts/jspm_packages/
     
-
     #⭐ Copy Wordpress from downloaded directory. Change Wordpress default directory. $_ = last argument passed to last command. Copy from downloaded wordpress (forked from official docker image - https://github.com/docker-library/wordpress/blob/master/docker-entrypoint.sh)
     mkdir -p /app/root/site;
     tar cf - --one-file-system -C /usr/src/wordpress . | tar xf - -C /app/root/site
@@ -107,8 +107,8 @@ else
     # It appears that data in volumes get "staff" as group, and changing permissions needs a workaround.
     chown -R www-data:www-data /app/;
     find /app/ -type f -exec chown -R www-data:www-data {} \;
-    find /app/ -type f -exec chmod -R 777 {} \;
-    find /app/ -type d -exec chmod -R 777 {} \;
+    find /app/ -type f -exec chmod -R 644 {} \;
+    find /app/ -type d -exec chmod -R 755 {} \;
     # https://github.com/boot2docker/boot2docker/issues/587 - change permissions and ownership from docker "staff" to "www-data"
     # There is staff, 1000, www-data groups and users when using volumes. (Should read more about them)
     usermod -u 1000 www-data
