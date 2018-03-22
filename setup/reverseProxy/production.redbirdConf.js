735ad97498b5aa42e7b2550e031e0c051d768003
Dr. Safi <sfmissive@gmail.com>
2018-03-22 22:11:53 +0200

    Update reverse proxy configuration.

--- setup/reverseProxy/production.redbirdConf.js
+++ setup/reverseProxy/production.redbirdConf.js
@@ -1,19 +1,20 @@
 // This file would be required in Redbird reverseProxy. 
 // USAGE: 
 
-module.exports = function reverseProxy(proxy) {
+module.exports = function reverseProxy() {
 
-    let email = process.env.EMAIL
     let domain = 'dentrist.com'
+    let containerGroupName = 'dentristwebapp'
 
     // TODO: Fix cross origin http in https, seems as if `upgrade` header doesn't work well in apache config wiht browser throgh http config file.
-    proxy.register(domain, 'http://dentristwebapp_wordpress:80', {
-        ssl: {
-            letsencrypt: {
-                email: email, // Domain owner/admin email
-                production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
-            }
-        }
-    });
-        
+    let proxyConfig = [
+        {
+            domain: domain,
+            containerRoute: `http://${containerGroupName}_wordpress:80`,
+            ssl: true            
+        },
+    ]
+
+    return proxyConfig
+
 }
